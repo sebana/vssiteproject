f50b25facb616f5ecdc59abffc9af4fc28f41ce0
DESKTOP-62774D9\JakeSon <honolrul98@gmail.com>
2019-08-09 17:28:57 +0900

    인물모델 완성

--- vsapp1/scripts/dataimport.py
+++ vsapp1/scripts/dataimport.py
@@ -2,26 +2,27 @@
 import csv
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from manage import DEFAULT_SETTINGS_MODULE
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", DEFAULT_SETTINGS_MODULE)
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vsproject.settings')
 
 import django
 django.setup() 
 
 from vsapp1.models import Person
-f = open('data_person.csv', 'r', encoding='utf-8')
+f = open('vsapp1/scripts/data_person.csv', 'r', encoding='utf-8')
 rdr = csv.reader(f)
 
-# for line in rdr:
-#     person = Person()
-#     person.name = rdr[0]
-#     person.gender = rdr[1]
-#     person.fav = 0
-#     person.image = ""
-#     person.degree = rdr[2]
-#     person.played = 0
-#     person.won = 0
+for line in rdr:
+    person = Person()
+    person.name = line[0]
+    person.gender = int(line[1])
+    person.fav = 0
+    person.image = ""
+    person.degree = int(line[2])
+    person.played = 0
+    person.won = 0
+    person.save()
 
 # def run():
 #     import csv
