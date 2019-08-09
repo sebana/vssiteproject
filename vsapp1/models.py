1b22043db10c6cb717b5c0ef82b4d833b66e919d
DESKTOP-62774D9\JakeSon <honolrul98@gmail.com>
2019-08-09 14:42:16 +0900

    models.py추가

--- vsapp1/models.py
+++ vsapp1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.TextField()
+    gender = models.IntegerField()
+    fav = models.IntegerField()
+    image = models.ImageField(blank = True)
+    degree = models.IntegerField()
+    played = models.IntegerField()
+    won = models.IntegerField()
+
+class Adjective(models.Model):
+    phrase = models.TextField()
+    fav = models.IntegerField()
+    degree = models.IntegerField()
+    played = models.IntegerField()
+    won = models.IntegerField()
--- vsapp1/views.py
+++ vsapp1/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from .models import Person
+import csv
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def data_import():
+    f = open('./person_data.csv', 'r', encoding='utf-8')
+    rdr = csv.reader(f)
+
+    for line in rdr:
+        person = Person()
+        person.name = rdr[0]
+        person.gender = rdr[1]
+        person.fav = 0
+        person.image = ""
+        person.degree = rdr[2]
+        person.played = 0
+        person.won = 0
